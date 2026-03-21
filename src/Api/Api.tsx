3a2cb8042b62e5166a94a56695c70f563ac4660f
DESKTOP-U096J61\dhaip <dhaipayannahadipya@gmail.com>
2026-03-21 16:35:00 +0600

    add netlify redirects

--- src/Api/Api.tsx
+++ src/Api/Api.tsx
@@ -2,7 +2,7 @@ import type { Habit } from "@/types/types";
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://localhost:5002",
+    baseURL: import.meta.env.VITE_API_URL || "https://habit-server-git-main-dhaipayannahas-projects.vercel.app",
 });
 
 
